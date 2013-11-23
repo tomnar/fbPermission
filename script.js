0d5c3371bfaf659cf63fe8e24bddacf84a28ee08
tomnar <tomquast89@gmail.com>
2013-11-23 13:31:01 -0800

    getting permissions string

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("script loaded");
+$(document).ready(function(){
+	window.setTimeout( checkFb, 200 ); // .2 seconds
+	
+	function checkFb(){
+		if(document.getElementById("platformDialogForm") != null){ //check if fb is on the page
+			console.log("script loaded");
+			var pmString = $('._5b_h').text();
+			console.log(pmString);
+		}
+	}
+});
